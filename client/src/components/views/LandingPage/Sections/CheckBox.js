6ac1b9d054aee7ae7346e79f406d5a5e36e435c3
mactto <shc9928@gmail.com>
2020-11-18 02:34:33 +0900

    udpates

---
+++ client/src/components/views/LandingPage/Sections/CheckBox.js
@@ -0,0 +1,41 @@
+import React, {useState} from 'react'
+import {Collapse, Checkbox} from 'antd';
+
+const {Panel} = Collapse;
+
+function CheckBox(props) {
+    const [checked, setChecked] = useState([]);
+    
+    const handleToggle = (value) => {
+        const currentIndex = checked.indexOf(value);
+        const newChecked = [...checked];
+
+        // state에 넣어주고
+        if (currentIndex == -1) {
+            newChecked.push(value);
+        } else { // 빼준다
+            newChecked.splice(currentIndex, 1);
+        }
+        setChecked(newChecked);
+        props.handleFilters(newChecked);
+    }
+
+    const renderCheckboxLists = () => props.list && props.list.map((value, index) => (
+        <React.Fragment key={index}>
+            <Checkbox onChange={() => handleToggle(value._id)} checked={checked.indexOf(value._id) === -1 ? false : true}/>
+                <span>{value.name}</span>
+        </React.Fragment>
+    ))
+
+    return (
+        <div>
+            <Collapse defaultActiveKey={['1']}>
+                <Panel header="This is panel header 1" key="1">
+                    {renderCheckboxLists()}
+                </Panel>
+            </Collapse>
+        </div>
+    )
+}
+
+export default CheckBox;
--- server/routes/product.js
+++ server/routes/product.js
@@ -37,11 +37,25 @@ router.post('/', (req, res) => {
 });
 
 router.post('/products', (req, res) => {
-    Product.find()
+    // mongodb 제한하기
+    // skip : 몇번째부터 가져올지, limit : 몇개까지 가져올지
+    let limit = req.body.limit ? parseInt(req.body.limit) : 100;
+    let skip = req.body.skip ? parseInt(req.body.skip) : 0;
+    let findArgs = {};
+
+    for (let key in req.body.filters) {
+        if (req.body.filters[key].length > 0) {
+            findArgs[key] = req.body.filters[key];
+        }
+    }
+
+    Product.find(findArgs)
     .populate("writer")
+    .skip(skip)
+    .limit(limit)
     .exec((err, productInfo) => {
         if (err) return res.status(400).json({success: false, err});
-        return res.status(200).json({success: true, productInfo});
+        return res.status(200).json({success: true, productInfo, postSize:productInfo.length});
     })
 })
 
